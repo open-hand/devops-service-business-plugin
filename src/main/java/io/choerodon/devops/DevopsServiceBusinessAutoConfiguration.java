05ec1911a746fc03833887f4df8f74d4182ecfbb
王灏 <hao.wang08@hand-china.com>
2021-12-16 11:43:16 +0800

    [imp] 代码调整

--- src/main/java/io/choerodon/devops/DevopsServiceBusinessAutoConfiguration.java
+++ src/main/java/io/choerodon/devops/DevopsServiceBusinessAutoConfiguration.java
@@ -1,34 +1,14 @@
 package io.choerodon.devops;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
 
-@EnableFeignClients("io.choerodon")
-@EnableEurekaClient
-@EnableDiscoveryClient
-@SpringBootApplication
-@EnableChoerodonResourceServer
-@EnableCircuitBreaker
-@EnableAsync
+@Configuration
+@ComponentScan
+@EnableFeignClients
 public class DevopsServiceBusinessAutoConfiguration {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DevopsServiceBusinessAutoConfiguration.class);
-
-    public static void main(String[] args) {
-        try {
-            SpringApplication.run(DevopsServiceBusinessAutoConfiguration.class, args);
-        } catch (Exception e) {
-            LOGGER.error("start error",e);
-        }
-    }
 
 }
